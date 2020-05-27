f8524a3f6cad8f0686dae84128ec32a58833c467
M1200 <54820873+M1200@users.noreply.github.com>
2020-05-27 14:05:16 -0600

    Upload Actualizar Contrasela
    Upload Actualizar Contraseña

---
+++ SourceCode/ActualizarContra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HUGO
+{
+    public partial class ActualizarContra : UserControl
+    {
+        public ActualizarContra()
+        {
+            InitializeComponent();
+        }
+
+        private void VolverMenu_Click(object sender, EventArgs e)
+        {
+            MainMenu main = new MainMenu();
+            main.Show();
+            this.Hide();
+        }
+    }
+}
